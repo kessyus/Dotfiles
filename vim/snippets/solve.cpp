226edfee9c5ec7fa080eafcec6e0db5b80aae0e3
Kessyus Fófano <kessyus@gmail.com>
2020-04-22 10:45:12 -0300

    added vim folder to the repo

---
+++ vim/snippets/solve.cpp
@@ -0,0 +1,16 @@
+/**
+ *      Author: Kessyus Fofano
+ *      Email:  kessyus@gmail.com
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  
+
+  return 0;
+}
